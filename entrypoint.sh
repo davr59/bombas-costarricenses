cd882bf9de1b002171aa15c33465c2c98123d177
David V <[davr59@outlook.com](mailto:davr59@outlook.com)>
2021-03-31 22:08:38 -0600

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 mkdir -p ${INPUT_BASE}.ask && \
-touch ${INPUT_BASE}.ask/cli_config && \
-echo ${INPUT_CLI_CONFIG} > ${INPUT_BASE}.ask/cli_config && \
-cd /app && \
-echo ${INPUT_SKILL_NAME} | ask init --hosted-skill-id ${INPUT_SKILL_ID} -p ${INPUT_PROFILE} && \
-cd ${INPUT_SKILL_NAME} && \
-git config --global user.name ${INPUT_USER_NAME} && \
-git config --global user.email ${INPUT_USER_EMAIL} && \
-git pull && \
-cp -rf /app/data ./ && \
-git add -A && \
-git commit -m 'update data' || echo "No changes to commit" && \
-git push
+ls
