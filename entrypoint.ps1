6e91dc473659dc80a7f5b89005d1a76eb6b51d5d
David V <[davr59@outlook.com](mailto:davr59@outlook.com)>
2021-04-10 13:39:19 -0600

    test

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -8,7 +8,7 @@ cd ${INPUT_SKILL_NAME} && \
 git config --global user.name ${INPUT_USER_NAME} && \
 git config --global user.email ${INPUT_USER_EMAIL} && \
 git pull && \
-cp -rf /app/data ./ && \
+cp -rf /app/data ./lambda/ && \
 git add -A && \
 git commit -m 'update data' || echo "No changes to commit" && \
 git push
